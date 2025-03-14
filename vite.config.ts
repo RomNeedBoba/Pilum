d6f3ef43e7da9626a293b386ac33e9ade5467d48
RomNeedBoba <rpphyrom.dev@gmail.com>
2025-03-14 21:50:56 +0700

    Again OMG

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Pilum/ ", // Set this to the root since you are using a custom domain
+  base: "/", // Set this to the root since you are using a custom domain
 })
